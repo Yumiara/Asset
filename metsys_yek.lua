eba5d1cb959192ab8c82d3eca429c2ace6048a0a
Yumiria <154666142+Yumiara@users.noreply.github.com>
2024-02-12 18:38:36 +0700

    Create metsys_yek.lua

---
+++ metsys_yek.lua
@@ -0,0 +1,74 @@
+-- Generated using RoadToGlory's Converter v1.1 (RoadToGlory#9879)
+
+-- Instances:
+
+local Converted = {
+	["_KeySystem"] = Instance.new("ScreenGui");
+	["_Frame"] = Instance.new("Frame");
+	["_UICorner"] = Instance.new("UICorner");
+	["_UIStroke"] = Instance.new("UIStroke");
+	["_ImageLabel"] = Instance.new("ImageLabel");
+	["_UICorner1"] = Instance.new("UICorner");
+	["_TextBox"] = Instance.new("TextBox");
+	["_TextButton"] = Instance.new("TextButton");
+	["_UIStroke1"] = Instance.new("UIStroke");
+}
+
+-- Properties:
+
+Converted["_KeySystem"].Name = "KeySystem"
+Converted["_KeySystem"].Parent = game:GetService("CoreGui")
+
+Converted["_Frame"].BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Converted["_Frame"].BorderColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_Frame"].BorderSizePixel = 3
+Converted["_Frame"].Position = UDim2.new(0.299921572, 0, 0.249653295, 0)
+Converted["_Frame"].Size = UDim2.new(0.400000006, 0, 0.5, 0)
+Converted["_Frame"].Parent = Converted["_KeySystem"]
+
+Converted["_UICorner"].Parent = Converted["_Frame"]
+
+Converted["_UIStroke"].Color = Color3.fromRGB(255, 0, 4.000000236555934)
+Converted["_UIStroke"].Thickness = 3
+Converted["_UIStroke"].Parent = Converted["_Frame"]
+
+Converted["_ImageLabel"].Image = "http://www.roblox.com/asset/?id=16323575491"
+Converted["_ImageLabel"].BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Converted["_ImageLabel"].BorderColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_ImageLabel"].BorderSizePixel = 0
+Converted["_ImageLabel"].Size = UDim2.new(1, 0, 1, 0)
+Converted["_ImageLabel"].Parent = Converted["_Frame"]
+
+Converted["_UICorner1"].Parent = Converted["_ImageLabel"]
+
+Converted["_TextBox"].CursorPosition = -1
+Converted["_TextBox"].Font = Enum.Font.SourceSans
+Converted["_TextBox"].Text = "Key"
+Converted["_TextBox"].TextColor3 = Color3.fromRGB(255, 255, 255)
+Converted["_TextBox"].TextScaled = true
+Converted["_TextBox"].TextSize = 14
+Converted["_TextBox"].TextWrapped = true
+Converted["_TextBox"].BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Converted["_TextBox"].BackgroundTransparency = 1
+Converted["_TextBox"].BorderColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_TextBox"].BorderSizePixel = 0
+Converted["_TextBox"].Position = UDim2.new(0.349581897, 0, 0.523578346, 0)
+Converted["_TextBox"].Size = UDim2.new(0.300000012, 0, 0.150000006, 0)
+Converted["_TextBox"].Parent = Converted["_Frame"]
+
+Converted["_TextButton"].Font = Enum.Font.SourceSans
+Converted["_TextButton"].Text = "Check Key"
+Converted["_TextButton"].TextColor3 = Color3.fromRGB(0, 255, 30.00000201165676)
+Converted["_TextButton"].TextScaled = true
+Converted["_TextButton"].TextSize = 14
+Converted["_TextButton"].TextWrapped = true
+Converted["_TextButton"].BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Converted["_TextButton"].BackgroundTransparency = 1
+Converted["_TextButton"].BorderColor3 = Color3.fromRGB(0, 0, 0)
+Converted["_TextButton"].BorderSizePixel = 0
+Converted["_TextButton"].Position = UDim2.new(0.349581897, 0, 0.754160821, 0)
+Converted["_TextButton"].Size = UDim2.new(0.300000012, 0, 0.150000006, 0)
+Converted["_TextButton"].Parent = Converted["_Frame"]
+
+Converted["_UIStroke1"].Color = Color3.fromRGB(13.00000112503767, 255, 0)
+Converted["_UIStroke1"].Parent = Converted["_TextButton"]
